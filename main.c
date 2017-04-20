6587c62cb8b77bcaad6089398c68f3df30fa1ed9
wisecs <wisecs@appstate.edu>
2017-04-20 14:24:04 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Final.c
+ *
+ * Created: 4/20/2017 2:23:04 PM
+ * Author : Carter W
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
